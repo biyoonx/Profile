454fe8b4cb9370d0ff5773c475793a3fec11efee
biyoonx <ok2geoya@gmail.com>
2023-12-22 18:46:45 +0900

    fix 404 error by changing the root path

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,36 @@ import CVPage from './pages/CVPage';
 import CoverLetterPage from './pages/CoverLetterPage';
 import ProjectPage from './pages/ProjectPage';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <Navigate replace to={'cv'} />,
+      children: [
+        {
+          path: '',
+          element: <Navigate replace to={'cv'} />,
+        },
+        {
+          path: 'cv',
+          element: <CVPage />,
+        },
+        {
+          path: 'cover_letter',
+          element: <CoverLetterPage />,
+        },
+        {
+          path: 'project',
+          element: <ProjectPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/Profile',
-    element: <RootLayout />,
-    errorElement: <Navigate replace to={'cv'} />,
-    children: [
-      {
-        path: '',
-        element: <Navigate replace to={'cv'} />,
-      },
-      {
-        path: 'cv',
-        element: <CVPage />,
-      },
-      {
-        path: 'cover_letter',
-        element: <CoverLetterPage />,
-      },
-      {
-        path: 'project',
-        element: <ProjectPage />,
-      },
-    ],
-  },
-]);
+    basename: '/Profile',
+  }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
