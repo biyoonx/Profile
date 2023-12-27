0fd1f8b7dce36bc720e15a3651a49330c6826f95
biyoonx <ok2geoya@gmail.com>
2023-12-27 13:06:20 +0900

    temp state change

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -65,12 +65,12 @@ export default function ProjectPage() {
         title={modeling.title}
         contents={() => <ProjectModeling contents={modeling.contents} />}
       />
-      <ContentsBox
+      {false && <ContentsBox
         title={troubleShooting.title}
         contents={() => (
           <ProjectTroubleShooting contents={troubleShooting.contents} />
         )}
-      />
+      />}
     </div>
   );
 }
